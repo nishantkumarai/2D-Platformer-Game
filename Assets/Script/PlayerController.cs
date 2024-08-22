4da29759467f1e7edd555c629b8ee2bdea372e3b
Nishant Kumar Rai <66172201+nishantkumarai@users.noreply.github.com>
2024-08-22 12:59:56 +0530

    Movement and  vertical input and jump

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -6,28 +6,68 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public Animator animator;
+    public float speed;
+    public float jump;
 
-    // Update is called once per frame
+    private Rigidbody2D rb2d;
+
+    private void Awake()
+    {
+        Debug.Log("Player controller awake");
+        rb2d = gameObject.GetComponent<Rigidbody2D>();
+    }
     void Update()
     {
-        float speed = Input.GetAxisRaw("Horizontal");
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Jump");
         bool crouch = Input.GetKey(KeyCode.LeftControl);
-        float jump = Input.GetAxisRaw("Vertical");
 
-        animator.SetFloat("Speed", Mathf.Abs(speed));
         animator.SetBool("Crouch", crouch);
-        animator.SetFloat("Jump", jump);
-        
+       
+
+        PlayMovementAnimation(horizontal, vertical);
+        MoveCharactor(horizontal, vertical);
+    }
+
+    private void MoveCharactor(float horizontal, float vertical)
+    {
+        //move charactor horizontally
+        Vector3 position = transform.position;
+        position.x += horizontal * speed * Time.deltaTime;
+        transform.position = position;
+
+        // move charactor vertically
+        if(vertical > 0)
+        {
+            rb2d.AddForce(new Vector2(0f, jump), ForceMode2D.Force);
+        }
+    }
+
+    private void PlayMovementAnimation(float horizontal, float vertical)
+    {
+        //run
+        animator.SetFloat("Speed", Mathf.Abs(horizontal));
 
         Vector3 scale = transform.localScale;
-        if (speed < 0)
+        if (horizontal < 0)
         {
             scale.x = -1f * Mathf.Abs(scale.x);
         }
-        else if (speed > 0)       
+        else if (horizontal > 0)
         {
-             scale.x = Mathf.Abs(scale.x);
+            scale.x = Mathf.Abs(scale.x);
         }
         transform.localScale = scale;
+
+        //jump 
+        
+        if (vertical > 0)
+        {
+            animator.SetBool("Jump", true);
+        }
+        else
+        {
+            animator.SetBool("Jump", false);
+        }
     }
 }
